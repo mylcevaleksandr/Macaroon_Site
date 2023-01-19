a84b0afcffdab9bfdb128078eba9f847c920cbf4
mylcevaleksandr <myltsevaleksandr@gmail.com>
2023-01-20 02:03:20 +0300

    Home Work UPD

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,9 +1,14 @@
+
+const body = document.querySelector('body');
+
 document.getElementById('menu-burger').onclick = function () {
     document.getElementById('menu-ul').classList.add('menu__open');
+    body.style.overflow = 'hidden';
 }
 
 document.querySelectorAll('#menu-ul *').forEach((item) => {
     item.onclick = () => {
         document.getElementById('menu-ul').classList.remove('menu__open');
+        body.style.overflow = 'auto';
     }
 })
